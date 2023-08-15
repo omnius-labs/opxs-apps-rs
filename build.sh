bc268c9c9ff98902dc24403674be3f303e8ca3bf
Lyrise <lyrise1984@gmail.com>
2023-08-15 09:44:20 +0900

    fix

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ rm -rf ./bin
 docker build -f ./Dockerfile.build . -t opxs-api-build-image
 docker run --name opxs-api-builder -d opxs-api-build-image
 docker cp opxs-api-builder:/app ./bin
+docker stop opxs-api-builder
 docker rm opxs-api-builder
