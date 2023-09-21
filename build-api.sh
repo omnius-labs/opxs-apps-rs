f1aa41c02697694f010a83472b58c22971a72a08
Lyrise <lyrise1984@gmail.com>
2023-09-22 02:36:37 +0900

    fix

--- build-api.sh
+++ build-api.sh
@@ -6,6 +6,6 @@ mkdir -p ./bin/api
 
 docker build -f ./Dockerfile.build.api . -t opxs-build-api-image
 docker run --name opxs-api-builder -d opxs-build-api-image
-docker cp opxs-api-builder:/app ./bin/api
+docker cp opxs-api-builder:/app/opxs-api ./bin/api/
 docker stop opxs-api-builder
 docker rm opxs-api-builder
--- build-batch.sh
+++ build-batch.sh
@@ -6,6 +6,7 @@ mkdir -p ./bin/batch
 
 docker build -f ./Dockerfile.build.batch . -t opxs-build-batch-image
 docker run --name opxs-batch-builder -d opxs-build-batch-image
-docker cp opxs-batch-builder:/app ./bin/batch
+docker cp opxs-batch-builder:/app/opxs-batch-send-email ./bin/batch
+docker cp opxs-batch-builder:/app/opxs-batch-send-email-feedback ./bin/batch
 docker stop opxs-batch-builder
 docker rm opxs-batch-builder
