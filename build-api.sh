c37e657c27ce41d48933198ad357e31760a445ae
Lyrise <lyrise1984@gmail.com>
2023-09-21 22:24:04 +0900

    fix

--- build-api.sh
+++ build-api.sh
@@ -2,9 +2,10 @@
 set -euo pipefail
 
 rm -rf ./bin/api
+mkdir -p ./bin/api
 
 docker build -f ./Dockerfile.build.api . -t opxs-build-api-image
 docker run --name opxs-api-builder -d opxs-build-api-image
-docker cp opxs-api-builder:/app ./bin
+docker cp opxs-api-builder:/app ./bin/api
 docker stop opxs-api-builder
 docker rm opxs-api-builder
--- build-batch.sh
+++ build-batch.sh
@@ -2,9 +2,10 @@
 set -euo pipefail
 
 rm -rf ./bin/batch
+mkdir -p ./bin/batch
 
 docker build -f ./Dockerfile.build.batch . -t opxs-build-batch-image
 docker run --name opxs-batch-builder -d opxs-build-batch-image
-docker cp opxs-batch-builder:/app ./bin
+docker cp opxs-batch-builder:/app ./bin/batch
 docker stop opxs-batch-builder
 docker rm opxs-batch-builder
