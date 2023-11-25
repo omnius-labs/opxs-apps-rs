5690eded8f68e23a554e06dcecd83f595a9bdddb
Lyrise <lyrise1984@gmail.com>
2023-11-25 23:00:07 +0900

    fix

--- entrypoints/api/src/interface/routes/auth/email.rs
+++ entrypoints/api/src/interface/routes/auth/email.rs
@@ -1,8 +1,4 @@
-use axum::{
-    extract::{Query, State},
-    routing::post,
-    Json, Router,
-};
+use axum::{extract::State, routing::post, Json, Router};
 use hyper::StatusCode;
 use serde::Deserialize;
 use url::Url;
