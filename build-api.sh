42609e85f41bdb053a10e546a3f39532896b5423
Lyrise <lyrise1984@gmail.com>
2023-12-31 22:17:04 +0900

    Refactor build scripts to stop and remove containers in one command

--- build-api.sh
+++ build-api.sh
@@ -7,5 +7,4 @@ mkdir -p ./bin/api
 docker build -f ./Dockerfile.build.api . -t opxs-api-builder-image
 docker run --name opxs-api-builder -d opxs-api-builder-image
 docker cp opxs-api-builder:/app/opxs-api ./bin/api/
-docker stop opxs-api-builder
-docker rm opxs-api-builder
+docker stop opxs-api-builder && docker rm opxs-api-builder
--- build-batch.sh
+++ build-batch.sh
@@ -8,5 +8,4 @@ docker build -f ./Dockerfile.build.batch . -t opxs-batch-builder-image
 docker run --name opxs-batch-builder -d opxs-batch-builder-image
 docker cp opxs-batch-builder:/app/opxs-batch-email-send ./bin/batch
 docker cp opxs-batch-builder:/app/opxs-batch-email-send-feedback ./bin/batch
-docker stop opxs-batch-builder
-docker rm opxs-batch-builder
+docker stop opxs-batch-builder && docker rm opxs-batch-builder
