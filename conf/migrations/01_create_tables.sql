bfdb063ae743b0a9a9975c59036a8d9a94943948
Lyrise <lyrise1984@gmail.com>
2023-12-31 22:16:43 +0900

    Update sql

--- conf/migrations/01_create_tables.sql
+++ conf/migrations/01_create_tables.sql
@@ -4,108 +4,122 @@ CREATE TYPE user_authentication_type AS ENUM ('Email', 'Provider');
 CREATE TYPE user_role AS ENUM ('Admin', 'User');
 
 CREATE TABLE users (
-    id BIGSERIAL PRIMARY KEY,
+    id VARCHAR(255) NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     authentication_type user_authentication_type NOT NULL,
     role user_role NOT NULL,
     created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-CREATE TRIGGER update_users_updated_at_step1 BEFORE UPDATE ON users FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_none();
-CREATE TRIGGER update_users_updated_at_step2 BEFORE UPDATE OF updated_at ON users FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_same();
-CREATE TRIGGER update_users_updated_at_step3 BEFORE UPDATE ON users FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_current();
+CREATE TRIGGER users_updated_at_step1 BEFORE UPDATE ON users FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_none();
+CREATE TRIGGER users_updated_at_step2 BEFORE UPDATE OF updated_at ON users FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_same();
+CREATE TRIGGER users_updated_at_step3 BEFORE UPDATE ON users FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_current();
 
--- users_auth_email
+-- user_auth_emails
 
-CREATE TABLE users_auth_email (
-    id BIGSERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    email_verified BOOLEAN NOT NULL DEFAULT FALSE,
+CREATE TABLE user_auth_emails (
+    email VARCHAR(255) NOT NULL PRIMARY KEY,
+    user_id VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
     salt VARCHAR(255) NOT NULL,
+    email_verified BOOLEAN NOT NULL DEFAULT FALSE,
     created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
-CREATE TRIGGER update_users_auth_email_updated_at_step1 BEFORE UPDATE ON users_auth_email FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_none();
-CREATE TRIGGER update_users_auth_email_updated_at_step2 BEFORE UPDATE OF updated_at ON users_auth_email FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_same();
-CREATE TRIGGER update_users_auth_email_updated_at_step3 BEFORE UPDATE ON users_auth_email FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_current();
+CREATE TRIGGER user_auth_emails_updated_at_step1 BEFORE UPDATE ON user_auth_emails FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_none();
+CREATE TRIGGER user_auth_emails_updated_at_step2 BEFORE UPDATE OF updated_at ON user_auth_emails FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_same();
+CREATE TRIGGER user_auth_emails_updated_at_step3 BEFORE UPDATE ON user_auth_emails FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_current();
 
--- users_auth_provider
+-- user_auth_providers
 
-CREATE TABLE users_auth_provider (
-    id BIGSERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL UNIQUE,
+CREATE TABLE user_auth_providers (
     provider_type VARCHAR(255) NOT NULL,
     provider_user_id VARCHAR(255) NOT NULL,
+    user_id VARCHAR(255) NOT NULL UNIQUE,
     created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(provider_type, provider_user_id),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
-CREATE UNIQUE INDEX users_auth_provider_provider_type_provider_user_id_unique_index ON users_auth_provider(provider_type, provider_user_id);
-CREATE TRIGGER update_users_auth_provider_updated_at_step1 BEFORE UPDATE ON users_auth_provider FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_none();
-CREATE TRIGGER update_users_auth_provider_updated_at_step2 BEFORE UPDATE OF updated_at ON users_auth_provider FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_same();
-CREATE TRIGGER update_users_auth_provider_updated_at_step3 BEFORE UPDATE ON users_auth_provider FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_current();
+CREATE UNIQUE INDEX user_auth_providers_provider_type_provider_user_id_unique_index ON user_auth_providers(provider_type, provider_user_id);
 
--- users_tokens
+-- refresh_tokens
 
-CREATE TABLE users_tokens (
-    id BIGSERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    refresh_token VARCHAR(255) NOT NULL UNIQUE,
+CREATE TABLE refresh_tokens (
+    refresh_token VARCHAR(255) NOT NULL PRIMARY KEY,
+    user_id VARCHAR(255) NOT NULL,
+    ip_address VARCHAR(255) NOT NULL,
+    user_agent VARCHAR(1024) NOT NULL,
     expires_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
-CREATE TRIGGER update_users_tokens_updated_at_step1 BEFORE UPDATE ON users_tokens FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_none();
-CREATE TRIGGER update_users_tokens_updated_at_step2 BEFORE UPDATE OF updated_at ON users_tokens FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_same();
-CREATE TRIGGER update_users_tokens_updated_at_step3 BEFORE UPDATE ON users_tokens FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_current();
+CREATE TRIGGER refresh_tokens_updated_at_step1 BEFORE UPDATE ON refresh_tokens FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_none();
+CREATE TRIGGER refresh_tokens_updated_at_step2 BEFORE UPDATE OF updated_at ON refresh_tokens FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_same();
+CREATE TRIGGER refresh_tokens_updated_at_step3 BEFORE UPDATE ON refresh_tokens FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_current();
 
 -- email_send_jobs
 
-CREATE TYPE email_send_job_type AS ENUM ('Unknown', 'EmailConfirm');
-CREATE TYPE email_send_job_status AS ENUM ('Unknown', 'Pending', 'Processing', 'Completed', 'Failed');
-
 CREATE TABLE email_send_jobs (
-    id BIGSERIAL PRIMARY KEY,
-    type email_send_job_type NOT NULL,
-    status email_send_job_status NOT NULL,
+    job_id VARCHAR(255) NOT NULL PRIMARY KEY,
+    batch_count INTEGER NOT NULL,
+    email_address_count INTEGER NOT NULL,
+    type VARCHAR(32) NOT NULL,
     param TEXT,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- email_send_job_batches
+
+CREATE TABLE email_send_job_batches (
+    job_id VARCHAR(255) NOT NULL,
+    batch_id INTEGER NOT NULL,
+    status VARCHAR(32) NOT NULL,
+    PRIMARY KEY(job_id, batch_id),
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TRIGGER email_send_job_batches_updated_at_step1 BEFORE UPDATE ON email_send_job_batches FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_none();
+CREATE TRIGGER email_send_job_batches_updated_at_step2 BEFORE UPDATE OF updated_at ON email_send_job_batches FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_same();
+CREATE TRIGGER email_send_job_batches_updated_at_step3 BEFORE UPDATE ON email_send_job_batches FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_current();
+
+-- email_send_job_batch_details
+
+CREATE TABLE email_send_job_batch_details (
+    job_id VARCHAR(255) NOT NULL,
+    batch_id INTEGER NOT NULL,
+    email_address VARCHAR(255) NOT NULL,
+    retry_count INTEGER NOT NULL,
+    message_id VARCHAR(255),
+    status VARCHAR(32) NOT NULL,
+    PRIMARY KEY(job_id, email_address, retry_count),
     failed_reason TEXT,
     created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-CREATE TRIGGER email_send_jobs_updated_at_step1 BEFORE UPDATE ON email_send_jobs FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_none();
-CREATE TRIGGER email_send_jobs_updated_at_step2 BEFORE UPDATE OF updated_at ON email_send_jobs FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_same();
-CREATE TRIGGER email_send_jobs_updated_at_step3 BEFORE UPDATE ON email_send_jobs FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_current();
+CREATE TRIGGER email_send_job_batches_updated_at_step1 BEFORE UPDATE ON email_send_job_batches FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_none();
+CREATE TRIGGER email_send_job_batches_updated_at_step2 BEFORE UPDATE OF updated_at ON email_send_job_batches FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_same();
+CREATE TRIGGER email_send_job_batches_updated_at_step3 BEFORE UPDATE ON email_send_job_batches FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_current();
+CREATE INDEX email_send_job_batch_details_job_id_batch_id_index ON email_send_job_batch_details(job_id, batch_id);
+CREATE INDEX email_send_job_batch_details_message_id_index ON email_send_job_batch_details(message_id);
 
--- email_send_addresses
+-- email_send_blocked_addresses
 
-CREATE TABLE email_send_addresses (
-    id BIGSERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    is_blocked BOOLEAN NOT NULL DEFAULT FALSE,
+CREATE TABLE email_send_blocked_addresses (
+    email_address VARCHAR(255) NOT NULL,
+    reason TEXT,
     created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+    PRIMARY KEY(email_address, created_at)
 );
-CREATE TRIGGER email_send_addresses_updated_at_step1 BEFORE UPDATE ON email_send_addresses FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_none();
-CREATE TRIGGER email_send_addresses_updated_at_step2 BEFORE UPDATE OF updated_at ON email_send_addresses FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_same();
-CREATE TRIGGER email_send_addresses_updated_at_step3 BEFORE UPDATE ON email_send_addresses FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_current();
 
--- email_send_logs
+-- email_send_event_logs
 
 CREATE TABLE email_send_logs (
-    id BIGSERIAL PRIMARY KEY,
-    email_send_address_id INTEGER NOT NULL,
+    message_id VARCHAR(255),
+    email_address VARCHAR(255) NOT NULL,
     event_type VARCHAR(32) NOT NULL,
     event_detail TEXT,
     created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (email_send_address_id) REFERENCES email_send_addresses(id) ON DELETE CASCADE
+    PRIMARY KEY(message_id, created_at)
 );
-COMMENT ON COLUMN email_send_logs.event_type IS 'bounce or spam';
-CREATE TRIGGER email_send_logs_updated_at_step1 BEFORE UPDATE ON email_send_logs FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_none();
-CREATE TRIGGER email_send_logs_updated_at_step2 BEFORE UPDATE OF updated_at ON email_send_logs FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_same();
-CREATE TRIGGER email_send_logs_updated_at_step3 BEFORE UPDATE ON email_send_logs FOR EACH ROW EXECUTE PROCEDURE refresh_updated_at_current();
