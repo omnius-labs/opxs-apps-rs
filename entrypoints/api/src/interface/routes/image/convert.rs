532605123c5a5bc5e382f32c349d585e2109796f
Lyrise <lyrise1984@gmail.com>
2024-01-26 09:34:14 +0900

    fix

--- entrypoints/api/src/interface/routes/image/convert.rs
+++ entrypoints/api/src/interface/routes/image/convert.rs
@@ -1,6 +1,5 @@
 use axum::{extract::State, routing::post, Json, Router};
-use opxs_auth::shared::model::AuthToken;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 use validator::Validate;
 
@@ -11,24 +10,27 @@ use crate::{
 
 #[allow(unused)]
 pub fn gen_service(state: AppState) -> Router {
-    Router::new().route("/refresh", post(refresh)).with_state(state)
+    Router::new().route("/upload", post(upload)).with_state(state)
 }
 
 #[utoipa::path(
     post,
-    path = "/api/v1/image/convert/",
-    request_body = RefreshInput,
+    path = "/api/v1/image/convert/upload",
+    request_body = UploadInput,
     responses(
         (status = 200, body = AuthToken)
     )
 )]
-pub async fn refresh(State(state): State<AppState>, ValidatedJson(input): ValidatedJson<RefreshInput>) -> Result<Json<AuthToken>, AppError> {
-    let auth_token = state.service.token.refresh(&input.refresh_token).await?;
-
-    Ok(Json(auth_token))
+pub async fn upload(State(_state): State<AppState>, ValidatedJson(_input): ValidatedJson<UploadInput>) -> Result<Json<UploadOutput>, AppError> {
+    todo!();
 }
 
 #[derive(Deserialize, ToSchema, Validate)]
-pub struct RefreshInput {
+pub struct UploadInput {
+    pub filename: String,
+}
+
+#[derive(Serialize, ToSchema, Validate)]
+pub struct UploadOutput {
     pub refresh_token: String,
 }
