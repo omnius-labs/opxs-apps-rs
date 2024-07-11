b98c0cc07dd6507adbc388b2f2ab80f75ef2fdfa
Lyrise <lyrise1984@gmail.com>
2024-07-12 08:24:17 +0900

    fix

--- build-api.sh
+++ build-api.sh
@@ -5,5 +5,5 @@ mkdir -p ./bin
 
 docker build -f ./Dockerfile.build.api --build-arg GIT_TAG="$USER-$(git describe --tags --always)" -t opxs-api-builder-image .
 docker run --name opxs-api-builder -d opxs-api-builder-image
-docker cp opxs-api-builder:/app/opxs-api ./bin/
+docker cp opxs-api-builder:/app/omnius-opxs-api ./bin/
 docker stop opxs-api-builder && docker rm opxs-api-builder
--- build-batch.sh
+++ build-batch.sh
@@ -5,5 +5,5 @@ mkdir -p ./bin
 
 docker build -f ./Dockerfile.build.batch-$1 --build-arg GIT_TAG="$USER-$(git describe --tags --always)" -t opxs-batch-$1-builder-image .
 docker run --name opxs-batch-$1-builder -d opxs-batch-$1-builder-image
-docker cp opxs-batch-$1-builder:/app/opxs-batch-$1 ./bin/
+docker cp opxs-batch-$1-builder:/app/omnius-opxs-batch-$1 ./bin/
 docker stop opxs-batch-$1-builder && docker rm opxs-batch-$1-builder
