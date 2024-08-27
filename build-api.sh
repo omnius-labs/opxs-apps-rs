bbcea1881a4be18dff37cc02fa8295bfdcdef163
Lyrise <lyrise1984@gmail.com>
2024-08-27 09:24:11 +0900

    fix

--- build-api.sh
+++ build-api.sh
@@ -3,7 +3,4 @@ set -euo pipefail
 
 mkdir -p ./bin
 
-docker build -f ./Dockerfile.build.api --build-arg GIT_TAG="$USER-$(git describe --tags --always)" -t opxs-api-builder-image .
-docker run --name opxs-api-builder -d opxs-api-builder-image
-docker cp opxs-api-builder:/app/omnius-opxs-api ./bin/
-docker stop opxs-api-builder && docker rm opxs-api-builder
+docker build -f ./Dockerfile.build.api --build-arg GIT_TAG="$USER-$(git describe --tags --always)" -t opxs-api-builder-image -o type=local,dest=bin .
